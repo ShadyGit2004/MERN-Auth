9274c1ba5c71e3c59337ddfb291478408d75971f
Rajat pandey <shadyaf6@gmail.com>
2025-11-24 00:06:14 +0530

    updated mongoDB url

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const databaseConnection = () => {
-    mongoose.connect(process.env.MONGODB_URL)
+  mongoose.connect(`${process.env.MONGODB_URL}/AuthenticationSystem`)
   .then(() => console.log('Connected to database!'))
   .catch((e)=>{console.log(e)});
 }
