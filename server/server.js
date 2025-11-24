2d29c8794145460ab6a2dc8c3e45aff4c6d0e8ba
Rajat pandey <shadyaf6@gmail.com>
2025-11-24 10:19:49 +0530

    added arr of origin

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 databaseConnection();
 
-const allowedOrigins = [process.env.CLIENT_URL];
+const allowedOrigins = [process.env.CLIENT_URL, 'http://localhost:5173'];
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
